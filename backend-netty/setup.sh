51c3623504d15eb06b677e25ee61eac8c2f587a0
mkim <mkim@microsoft.com>
2017-08-31 23:42:04 -0700

    type

--- backend-netty/setup.sh
+++ backend-netty/setup.sh
@@ -13,6 +13,6 @@ sysctl -w kernel.shmall=2097152
 
 
 openssl req -extensions v3_ca -new -x509 -days 30 -nodes -subj "/CN=NettyTestRoot" -newkey rsa:2048 -sha512 -out server_ca.pem -keyout server_ca.key
-openssl req -new -keyout server.key -nodes -newkey rsa:2048 -subj "/CN=apim-httpperf.westus.cloudapp.azure.com" | openssl x509 -req -CAke y server_ca.key -CA server_ca.pem -days 36500 -set_serial $RANDOM -sha512 -out server.pem
+openssl req -new -keyout server.key -nodes -newkey rsa:2048 -subj "/CN=apim-httpperf.westus.cloudapp.azure.com" | openssl x509 -req -CA key server_ca.key -CA server_ca.pem -days 36500 -set_serial $RANDOM -sha512 -out server.pem
 
 java -server -XX:+UseNUMA -XX:+UseParallelGC -XX:+AggressiveOpts -jar backend-netty-0.1-jar-with-dependencies.jar &
